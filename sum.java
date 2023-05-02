96d85b67835e2a1f9157fb119507709051f2115d
Adarsh Gupta <adarsh.gupta@aspiresys.com>
2023-05-02 16:59:08 +0530

    added result message

--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ public class sum{
     public static void main(string args[]){
         int a= 5;
         int b=4;
-        System.out.println(a+b);
+        System.out.println("the sum i" + (a+b));
     }
 }
